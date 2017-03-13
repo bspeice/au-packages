30743919a9255c9fc36554d292b23348a4760723
Bradlee Speice <bradlee.speice@gmail.com>
2017-03-13 07:00:59 -0400

    Initial config for Reaper, broken at the moment

---
+++ reaper/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'reaper'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32      = 'http://reaper.fm/files/5.x/reaper535-install.exe'
+$checksum32 = ''
+$url64      = 'http://reaper.fm/files/5.x/reaper535_x64-install.exe'
+$checksum64 = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'reaper*'
+
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
+
+  silentArgs   = '/S'
+  validExitCodes= @(0, 1223)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ reaper/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+$releases = 'http://reaper.fm/download.php'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $re  = "reaper.*-install.exe"
+    $installer = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $version_compact = $installer -split 'reaper|-' | select -Last 1 -Skip 1
+    $version_minor = $version_compact -split '5',2 -join '.'
+    $version = "5" + $version_minor
+
+    $url32 = 'http://reaper.fm/files/5.x/reaper' + $version_compact + '-install.exe'
+    $url64 = 'http://reaper.fm/files/5.x/reaper' + $version_compact + '_x64-install.exe'
+
+    return @{ URL64 = $url64; URL32 = $url32; Version = $version }
+}
+
+update
