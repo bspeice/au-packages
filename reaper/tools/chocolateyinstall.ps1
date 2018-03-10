16ee2e72c964deb741672b6b5695614a91008a9a
Bradlee Speice <bradlee.speice@gmail.com>
2018-03-10 01:12:47 +0000

    AU: 1 updated - reaper [skip ci]

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'reaper'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'http://dlcf.reaper.fm/5.x/reaper576-install.exe'
-$checksum32 = '7dc3bf9f1adffd8cfe57bf1eec3d960f3a8d2d20358afcbdbc095ced57819431'
-$url64      = 'http://dlcf.reaper.fm/5.x/reaper576_x64-install.exe'
-$checksum64 = 'b2e037ef344bf7c64916a9b4df02c25bfbe8515ef26332e391a6358ad271ff1e'
+$url32      = 'http://dlcf.reaper.fm/5.x/reaper577-install.exe'
+$checksum32 = 'e825419c02d908c97dfedee2a821273315582dff37852eab5fd995068b93ac5e'
+$url64      = 'http://dlcf.reaper.fm/5.x/reaper577_x64-install.exe'
+$checksum64 = 'b7f7bd9f19fae349ef2bf8f823e5337447f583d74f03ef6c8bfd66a81c67f073'
 
 $packageArgs = @{
   packageName   = $packageName
