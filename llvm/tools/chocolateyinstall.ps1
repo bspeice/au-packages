5035a059916e9811b0323dcd3024d68798d5c4db
Bradlee Speice <bradlee.speice@gmail.com>
2019-03-24 23:53:13 -0400

    LLVM 8.0

--- llvm/tools/chocolateyinstall.ps1
+++ llvm/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'llvm'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://releases.llvm.org/7.0.1/LLVM-7.0.1-win32.exe'
-$checksum32 = 'd56d8716da871c1895646b47aa406b27045d7155efdb4c5425dbe28b41df90b3'
-$url64      = 'https://releases.llvm.org/7.0.1/LLVM-7.0.1-win64.exe'
-$checksum64 = '672e4c420d6543a8a9f8ec5f1e5f283d88ac2155ef4c57232a399160a02bff57'
+$url32      = 'https://releases.llvm.org/8.0.0/LLVM-8.0.0-win32.exe'
+$checksum32 = '43ff5543c02873f41b87508c90509946293fe613fdbc57f8d19349e931aad6da'
+$url64      = 'https://releases.llvm.org/8.0.0/LLVM-8.0.0-win64.exe'
+$checksum64 = '56d582eed2d5def6accaedabbe11ae368186600798e2a6a7eb86a727fa7bb20c'
 
 $packageArgs = @{
   packageName   = $packageName
