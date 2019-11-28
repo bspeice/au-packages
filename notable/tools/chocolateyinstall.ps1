363498c9d847540f057d8f363cf219c2adc91338
Bradlee Speice <bradlee.speice@gmail.com>
2019-11-27 22:36:03 -0500

    Notable 1.8.2

--- notable/tools/chocolateyinstall.ps1
+++ notable/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'notable'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://github.com/fabiospampinato/notable/releases/download/v1.8.1/Notable.Setup.1.8.1.exe'
-$checksum32 = '981d4bc68911466fa77dfbe7539d6a38dc07986ff8aa9349db3f7f82a5c00b72'
+$url32      = 'https://github.com/fabiospampinato/notable/releases/download/v1.8.2/Notable.Setup.1.8.2.exe'
+$checksum32 = 'ec72c75c27dba0d965dce7ce4ea114cd4f6febc3a3c92816f8d5dc41ee7baa65'
 
 $packageArgs = @{
   packageName   = $packageName
