7a5f42ec28c420947f3b714dd99e137705565847
Jerome Benoit <jerome.benoit@sap.com>
2019-12-19 20:54:45 +0100

    reaper: use $env:ChocolateyPackageName
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'reaper'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32      = 'http://dlcf.reaper.fm/6.x/reaper602-install.exe'
 $checksum32 = 'd612ea4297b8b9174c541972854955db6d737773ba2f00688fece95f8a1fd711'
@@ -9,7 +8,7 @@ $url64      = 'http://dlcf.reaper.fm/6.x/reaper602_x64-install.exe'
 $checksum64 = '18aad54087f90b8185d2d2cf9522b1b0e2e2480061675098069793d733e87087'
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
   url           = $url32
   url64bit      = $url64
