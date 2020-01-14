37d8d34383fbb68526095bf957a120b4e565a5f9
Jerome Benoit <jerome.benoit@sap.com>
2020-01-14 10:28:27 +0100

    Update reaper to 6.03.
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'http://dlcf.reaper.fm/6.x/reaper602-install.exe'
-$checksum32 = 'd612ea4297b8b9174c541972854955db6d737773ba2f00688fece95f8a1fd711'
-$url64      = 'http://dlcf.reaper.fm/6.x/reaper602_x64-install.exe'
-$checksum64 = '18aad54087f90b8185d2d2cf9522b1b0e2e2480061675098069793d733e87087'
+$url32      = 'http://dlcf.reaper.fm/6.x/reaper603-install.exe'
+$checksum32 = '323a9f64f4cac5de6c307e171bb9371cf76b280c1d1eca20d3e4f1ae8e340cb9'
+$url64      = 'http://dlcf.reaper.fm/6.x/reaper603_x64-install.exe'
+$checksum64 = '3ed9a93483aca98964c058827bebff9847fe8704baf9af259bbdc26f43f6472b'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
