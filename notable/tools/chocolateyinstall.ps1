e1db22e5365960fdee50856b57f345a6ab02abca
Jerome Benoit <jerome.benoit@sap.com>
2020-01-19 15:00:17 +0100

    Update notable to latest.
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- notable/tools/chocolateyinstall.ps1
+++ notable/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://github.com/fabiospampinato/notable/releases/download/v1.8.2/Notable.Setup.1.8.2.exe'
-$checksum32 = 'ec72c75c27dba0d965dce7ce4ea114cd4f6febc3a3c92816f8d5dc41ee7baa65'
+$url32      = 'https://github.com/fabiospampinato/notable/releases/download/v1.8.3/Notable.Setup.1.8.3.exe'
+$checksum32 = 'f5631b7e844e10d269f101cf6b4db99771fc7eff0984991a9927f087c6e999aa'
 
 $packageArgs = @{
   packageName   = $packageName
