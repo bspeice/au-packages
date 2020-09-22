b2f7d0e92736454f682f63fac8ab76f5aa0bc58c
Jérôme Benoit <jerome.benoit@piment-noir.org>
2020-09-22 23:47:15 +0200

    Reaper update to 6.14
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'http://dlcf.reaper.fm/6.x/reaper613-install.exe'
-$checksum32 = '09edf6865bee7f4ccb86cd97671aa23bfab615ec081d996a3d51e29c845385f9'
-$url64      = 'http://dlcf.reaper.fm/6.x/reaper613_x64-install.exe'
-$checksum64 = 'f2d8628f60425c58ac2f134e91c1c89bc48db6f239763595161cb6986f4a231a'
+$url32      = 'http://dlcf.reaper.fm/6.x/reaper614-install.exe'
+$checksum32 = '9e4728fd7b49beb04b4c2ecabadb064b2a443a8aff9a195e932442e1d282216d'
+$url64      = 'http://dlcf.reaper.fm/6.x/reaper614_x64-install.exe'
+$checksum64 = 'e76c390d77014935d04d125a593558758ca3bd0e4cea468a591cee7482d6d08d'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
