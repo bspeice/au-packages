0c3de2e091ebc03f018ea75fb785bde4aa337d5c
Jérôme Benoit <jerome.benoit@piment-noir.org>
2021-05-15 12:23:41 +0200

    Fix sonic visualiser download link
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- sonicvisualiser/update.ps1
+++ sonicvisualiser/update.ps1
@@ -5,9 +5,9 @@ $releases = 'https://www.sonicvisualiser.org/download.html'
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $regex   = '.*-win32.msi$'
-    $url     = $download_page.Links | ? href -match $regex | Select-Object -Skip 1 -expand href
+    $url     = $download_page.Links | ? href -match $regex | Select-Object -First 1 -expand href
     $regex64 = '.*-win64.msi$'
-    $url64   = $download_page.Links | ? href -match $regex64 | Select-Object -Skip 1 -expand href
+    $url64   = $download_page.Links | ? href -match $regex64 | Select-Object -First 1 -expand href
     $version = $url64 -split '-|.msi' | Select-Object -Last 1 -Skip 2
     # Write-Host $url
     # Write-Host $url64
