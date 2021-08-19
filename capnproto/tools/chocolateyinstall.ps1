7281f441d9ba625b2c406637ca7e2211383ca5d1
Jérôme Benoit <jerome.benoit@piment-noir.org>
2021-08-19 20:18:03 +0200

    Update Cap'n Proto to 0.9.0
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- capnproto/tools/chocolateyinstall.ps1
+++ capnproto/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32      = 'https://capnproto.org/capnproto-c++-win32-0.8.0.zip'
-$checksum32 = 'e008fa75e75c39cafbadca07d2054d579477daee82e4f7a39784f27cfd2e7c77'
+$url32      = 'https://capnproto.org/capnproto-c++-win32-0.9.0.zip'
+$checksum32 = 'cb7b2783ec9260cd07bc99fb427902810b5351958da92b2647cdc2a72b30fd54'
 
 $packageArgs = @{
   packageName   = $packageName
