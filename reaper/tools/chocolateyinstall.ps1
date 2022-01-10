89124e906996f9d4fc4649fb922d1b09fbf76dde
Jérôme Benoit <jerome.benoit@piment-noir.org>
2022-01-10 11:44:39 +0100

    Update reaper to 6.45
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dlcf.reaper.fm/6.x/reaper643-install.exe'
-$checksum32 = '9f0b869f4125b20d0a18564ea02f7e1e2731a99c99601907af9d707dec2a019a'
-$url64 = 'https://dlcf.reaper.fm/6.x/reaper643_x64-install.exe'
-$checksum64 = 'a92df108a0ce8c10457e626c6e07d75dba8ddf330a541aa506b67ac1b4f0ae1c'
+$url32 = 'https://dlcf.reaper.fm/6.x/reaper645-install.exe'
+$checksum32 = '223430c28e5a56168676957aae09702ad4386c95ae033c3bf1085cb83d6019ab'
+$url64 = 'https://dlcf.reaper.fm/6.x/reaper645_x64-install.exe'
+$checksum64 = '8ad3d3b37a850f1f5cb8da28657cb2e7aa4f9d68d675b391cc6cd512316bc549'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
