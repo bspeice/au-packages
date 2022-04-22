a4c70dd03cae13b1c693eae96b7c17123cd23790
Jérôme Benoit <jerome.benoit@piment-noir.org>
2022-04-22 19:57:38 +0200

    Update reaper to 5.54
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dlcf.reaper.fm/6.x/reaper653-install.exe'
-$checksum32 = 'fdf2db27cfc8ec1672056af9650a59ea0f4196e507e50c6369fcbca73432b93c'
-$url64 = 'https://dlcf.reaper.fm/6.x/reaper653_x64-install.exe'
-$checksum64 = '3db1321d54c5a17f27a639051fa960339443a46d88249cf6882bafccb5469790'
+$url32 = 'https://dlcf.reaper.fm/6.x/reaper654-install.exe'
+$checksum32 = '92b6000858e8c636fdb2766480b616cae59edda1a266103cc686f45074c08ce7'
+$url64 = 'https://dlcf.reaper.fm/6.x/reaper654_x64-install.exe'
+$checksum64 = '185eeca81f139b1d6d146c612eebc5b91fab837b56952202f185f2346c5f9629'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
