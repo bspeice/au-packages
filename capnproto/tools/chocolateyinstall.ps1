2a25da7a20dcfeefd997291dc7ea7ed0c85895cf
Bradlee Speice <bradlee@speice.io>
2022-06-11 14:17:39 -0400

    Capnproto 0.10.1

--- capnproto/tools/chocolateyinstall.ps1
+++ capnproto/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://capnproto.org/capnproto-c++-win32-0.9.1.zip'
-$checksum32 = '0fdd8df285272c966947dc8bea6fb62a06269182a415ae76ef7b88ab3c200e76'
+$url32 = 'https://capnproto.org/capnproto-c++-win32-0.10.1.zip'
+$checksum32 = '6a8ee3fe6d7289a31ca95a626926b2af885ee4352a4cc6d10721d8756d1ea33a'
 
 $packageArgs = @{
   packageName   = $packageName
