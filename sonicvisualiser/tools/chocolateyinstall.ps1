decdfd1a995ee5e6089a28a9ca45d488b5f3cad4
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-05-21 21:12:05 +0200

    Update sonicvisualizer to 4.5.2
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- sonicvisualiser/tools/chocolateyinstall.ps1
+++ sonicvisualiser/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://code.soundsoftware.ac.uk/attachments/download/2840/sonic-visualiser-4.5.1-win32.msi'
-$url64 = 'https://code.soundsoftware.ac.uk/attachments/download/2839/sonic-visualiser-4.5.1-win64.msi'
+$url = 'https://code.soundsoftware.ac.uk/attachments/download/2849/sonic-visualiser-4.5.2-win32.msi'
+$url64 = 'https://code.soundsoftware.ac.uk/attachments/download/2848/sonic-visualiser-4.5.2-win64.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName   = 'sonicvisualiser*'
 
-  checksum       = 'b45fdd1903a90a52285373070a895e8b2bc992e0539da15366aa23e80d044b88'
+  checksum       = '79845c404523990dd6baabdc7ac9ba25156b34d73a565d7ebdcb6f3aae8ec02f'
   checksumType   = 'sha256'
-  checksum64     = '551f1409e989ca29fe0716621726ffa0498abd06edb075277b4c4494e6d13f25'
+  checksum64     = 'fc6bd67628b982a7e3e80faaea25e3ddfe991dec167f0b86002b6545a43d8770'
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
