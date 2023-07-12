33512512afdd625cb98fc2c282131708bff9a540
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-07-12 22:41:55 +0200

    Update reaper to 6.81
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dlcf.reaper.fm/6.x/reaper680-install.exe'
-$checksum32 = '6284df60970df94ce18a815a9bf7f279acd10cb9265c68457729880b3e58a558'
-$url64 = 'https://dlcf.reaper.fm/6.x/reaper680_x64-install.exe'
-$checksum64 = '1288c18a00468822374ced5f5ef735b58ac595a23bbb6706cac6557f313231ef'
+$url32 = 'https://dlcf.reaper.fm/6.x/reaper681-install.exe'
+$checksum32 = '6c5d9075f542234dfc3e5a4aeaf8d0667d01349b258b326ab80645605230789f'
+$url64 = 'https://dlcf.reaper.fm/6.x/reaper681_x64-install.exe'
+$checksum64 = '1910b2ba20a9ec55a2a1624be11deb33d773bbe679bd5cef614074a80577a072'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
