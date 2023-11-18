fc4de3aa6a86bf75bf58b2b998d73064b1748191
Jérôme Benoit <jerome.benoit@piment-noir.org>
2023-11-18 19:55:36 +0100

    Update reaper to 7.05
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dlcf.reaper.fm/7.x/reaper703-install.exe'
-$checksum32 = '4e201cb33b3e521880708283a744192da635000f9c20bc44e9e9434f64cc97d9'
-$url64 = 'https://dlcf.reaper.fm/7.x/reaper703_x64-install.exe'
-$checksum64 = '52b522798aedefbd81844e30ad21dbf4015c64cfa865b090b122b15a2f049d54'
+$url32 = 'https://dlcf.reaper.fm/7.x/reaper705-install.exe'
+$checksum32 = 'c6ee4283eb390744929e719a1ab620d8aa2e1edd559bd7776d1c43f5dd94557b'
+$url64 = 'https://dlcf.reaper.fm/7.x/reaper705_x64-install.exe'
+$checksum64 = 'cbecde7d1e184aca0159c3c8b348d01c09d501ea59af268bca76f501b33f77ff'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
