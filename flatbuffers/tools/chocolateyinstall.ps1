8fc7cbb8fc6aed8dd617e72c68582d76775ff6dc
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-06-19 17:48:11 +0200

    fix: ensure flatbuffers id is backward compatible with existing one
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- flatbuffers/tools/chocolateyinstall.ps1
+++ flatbuffers/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = $env:ChocolateyPackageName
-$softwareName = 'Flatbuffers*'
+$softwareName = 'flatc*'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url32 = 'https://github.com/google/flatbuffers/releases/download/v24.3.25/Windows.flatc.binary.zip'
 $checksum32 = '6455f5b6272b908dad073721e21b11720a9fddbae06e28b5c75f8ec458e7fe30'
