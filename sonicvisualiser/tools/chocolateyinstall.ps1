e3807b64198eb70189424f8824d25c6c319a615a
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-08-02 22:26:58 +0200

    Bump sonicvisualiser to 5.0.0
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- sonicvisualiser/tools/chocolateyinstall.ps1
+++ sonicvisualiser/tools/chocolateyinstall.ps1
@@ -2,21 +2,17 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://code.soundsoftware.ac.uk/attachments/download/2849/sonic-visualiser-4.5.2-win32.msi'
-$url64 = 'https://code.soundsoftware.ac.uk/attachments/download/2848/sonic-visualiser-4.5.2-win64.msi'
+$url64 = 'https://code.soundsoftware.ac.uk/attachments/download/2860/sonic-visualiser-5.0.0-win64.msi'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = $url
   url64bit       = $url64
 
   softwareName   = 'sonicvisualiser*'
 
-  checksum       = '79845c404523990dd6baabdc7ac9ba25156b34d73a565d7ebdcb6f3aae8ec02f'
-  checksumType   = 'sha256'
-  checksum64     = 'fc6bd67628b982a7e3e80faaea25e3ddfe991dec167f0b86002b6545a43d8770'
+  checksum64     = '8f0486063e36274bf537daf0ea766d9c19492c259c8da57333c8bc73311af303'
   checksumType64 = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- sonicvisualiser/update.ps1
+++ sonicvisualiser/update.ps1
@@ -4,26 +4,21 @@ $releases = 'https://www.sonicvisualiser.org/download.html'
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $regex = '.*-win32.msi$'
-  $url = $download_page.Links | ? href -match $regex | Select-Object -First 1 -expand href
   $regex64 = '.*-win64.msi$'
   $url64 = $download_page.Links | ? href -match $regex64 | Select-Object -First 1 -expand href
   $version = $url64 -split '-|.msi' | Select-Object -Last 1 -Skip 2
-  # Write-Host $url
   # Write-Host $url64
   # Write-Host $version
-  return @{ Version = $version; URL32 = $url; URL64 = $url64 }
+  return @{ Version = $version; URL64 = $url64 }
 }
 
 function global:au_SearchReplace {
   @{
     "tools\chocolateyInstall.ps1" = @{
-      "(^[$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-      "(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
       "(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
   }
 }
 
-Update-Package
+Update-Package -ChecksumFor 64
