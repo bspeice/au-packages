4ced9ad68bed7bf5cdbfd4a1f950857236d9add4
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-08-25 18:32:22 +0200

    Update tuxguitar to 1.6.4
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- tuxguitar/tools/chocolateyinstall.ps1
+++ tuxguitar/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/helge17/tuxguitar/releases/download/1.6.3/tuxguitar-1.6.3-windows-swt-x86_64-installer.exe'
+$url64 = 'https://github.com/helge17/tuxguitar/releases/download/1.6.4/tuxguitar-1.6.4-windows-swt-x86_64-installer.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   softwareName   = 'tuxguitar*'
 
-  checksum64       = 'a62e1b045fa6b2b6b931b2d718a6b2b874f4b679aeef214b04d6bcebc5938575'
+  checksum64       = 'b40984b5d9530c254f1e942a0f48f976892b7ae07f7bf205212efc45aba3e3ef'
   checksumType64   = 'sha256'
 
   silentArgs     = '--mode unattended --unattendedmodeui none'
