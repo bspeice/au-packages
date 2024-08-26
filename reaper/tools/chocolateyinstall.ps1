d77b5c531793243dcc2ba4b962e1cdd7ae33e82f
Jérôme Benoit <jerome.benoit@piment-noir.org>
2024-08-26 18:31:33 +0200

    Update reaper to 7.22
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dlcf.reaper.fm/7.x/reaper721-install.exe'
-$checksum32 = '5a54657c0bee2f45eeffc571c6f5de6e82092e5500594f46fd3cddbd1b3d98d9'
-$url64 = 'https://dlcf.reaper.fm/7.x/reaper721_x64-install.exe'
-$checksum64 = 'b53cf43be8425c070428a4ea8c30f1f791a43b355ac7492bcf18b6af26fe7084'
+$url32 = 'https://dlcf.reaper.fm/7.x/reaper722-install.exe'
+$checksum32 = '539c8191020830b5b68dd7b11f4a7e7ff38bd362d70c2b3da8cf152506aea7a8'
+$url64 = 'https://dlcf.reaper.fm/7.x/reaper722_x64-install.exe'
+$checksum64 = 'ab4ff5743ad846f9d8b3c084cb4edd09cfeedd459ceacbe9c4545b105d343436'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
