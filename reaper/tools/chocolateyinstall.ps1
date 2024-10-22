f1b2fd394e933a9980e49cfc8cd05a79c433ec8a
Bradlee Speice <bradlee@speice.io>
2024-10-22 19:58:07 -0400

    Reaper 7.8.25

--- reaper/tools/chocolateyinstall.ps1
+++ reaper/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://dlcf.reaper.fm/7.x/reaper722-install.exe'
-$checksum32 = '539c8191020830b5b68dd7b11f4a7e7ff38bd362d70c2b3da8cf152506aea7a8'
-$url64 = 'https://dlcf.reaper.fm/7.x/reaper722_x64-install.exe'
-$checksum64 = 'ab4ff5743ad846f9d8b3c084cb4edd09cfeedd459ceacbe9c4545b105d343436'
+$url32 = 'https://dlcf.reaper.fm/7.x/reaper725-install.exe'
+$checksum32 = 'fd96c0463489b99a22a9d34056caa4e0b86bef52035f81f5e9f4f3f979752f07'
+$url64 = 'https://dlcf.reaper.fm/7.x/reaper725_x64-install.exe'
+$checksum64 = '2294aefd0de4d577c465b53518c3a275a83fee45b56506f97b8d4b2930b7f278'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
